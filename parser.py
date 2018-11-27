90fb06b064c7ad7f1b25d140d8aabd477fcac4b9
Julian L Euler <julinksky@googlemail.com>
2018-11-27 15:10:35 +0100

    Added Exceptions to `save_corpus` added `list_corpora` added `remove_corpus`

--- parser.py
+++ parser.py
@@ -9,8 +9,19 @@ def read_corpus(filename):
         raise fnf
 
 def save_corpus(filename, corpus):
+    if filename is "":
+        raise ValueError("Invalid filename! Name is empty")
     if os.path.exists(os.path.join('corpora', filename + '.txt')):
-        raise FileExistsError
-    with open(os.path.join('corpora', filename + '.txt')) as file:
+        raise FileExistsError("This name already exists! Please enter a different name.")
+    with open(os.path.join('corpora', filename + '.txt'), 'w') as file:
         file.write(corpus)
     return
+
+def remove_corpus(filename):
+    try:
+        os.remove(os.path.join('corpora', filename + '.txt'))
+    except FileNotFoundError as fnf:
+        raise fnf
+
+def list_corpora():
+    return [f[:-4] for f in os.listdir('corpora') if os.path.isfile(os.path.join('corpora', f))]
