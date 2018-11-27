fc5c3634efd4f3b39c0476be2e31bb0b9d1c9ef2
Julian L Euler <julinksky@googlemail.com>
2018-11-27 15:21:05 +0100

    Changed font sizes of option box and generate button

--- gui.py
+++ gui.py
@@ -38,13 +38,13 @@ with gui("Markov Word Generator", bg="lightblue") as app:
     app.label("title1", value="Your word is:", sticky="e", font=25)
     app.label("gen_output", value="", row=0, column=1, sticky="", font=25)
     app.button("generate", generate, label="Generate new Word",
-                colspan=2, stretch="column", sticky="news")
+                colspan=2, stretch="column", sticky="news", font=15)
     app.spin("order", label="n=", value=0, endValue=5, pos=2, change=update_gen)
     app.spin("maxLength", label="Maximum Length:", value=2, endValue=20,
                 pos=7, row=2, column=1, change=update_gen)
     app.separator(colspan=2, stretch="both")
     app.option("corpora", value=parser.list_corpora(), label="Corpora",
-                colspan=2, change=update_corpora, stretch="column")
+                colspan=2, change=update_corpora, stretch="column", font=14)
     app.text("corpus", value=get_corpus(), scroll=True,
                 colspan=2, change=update_gen)
     app.entry("corpus_name", label="Corpus Name:", colspan=2)
